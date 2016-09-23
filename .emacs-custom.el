d4ba06ae3798e34a347ab44a45b2d2e9215089e9
Antoine Busque <antoine.busque@pillrcompany.com>
2016-09-22 23:31:19 -0400

    Update .emacs-custom.el for emacs 25
    Signed-off-by: Antoine Busque <antoine.busque@pillrcompany.com>

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -5,3 +5,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "black" :foreground "gray88" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "unknown" :family "Terminus")))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (irfc php-mode yaml-mode web-mode use-package nginx-mode markdown-mode json-mode html5-schema haskell-mode flycheck autopair ag))))
