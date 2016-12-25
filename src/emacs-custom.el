d987278758052b101c86efbf7ef6bd55c66e63c2
Antoine Busque <antoine.busque@pillrcompany.com>
2016-12-25 02:40:21 -0500

    emacs: add geben package for php debugging
    Signed-off-by: Antoine Busque <antoine.busque@pillrcompany.com>

--- src/emacs-custom.el
+++ src/emacs-custom.el
@@ -12,4 +12,4 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (groovy-mode csv-mode adoc-mode irfc php-mode yaml-mode web-mode use-package nginx-mode markdown-mode json-mode html5-schema haskell-mode flycheck autopair ag))))
+    (geben groovy-mode csv-mode adoc-mode irfc php-mode yaml-mode web-mode use-package nginx-mode markdown-mode json-mode html5-schema haskell-mode flycheck autopair ag))))
