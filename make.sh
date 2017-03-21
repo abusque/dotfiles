76f684a2d15ebac15d6f906366d3e088c14953e9
Antoine Busque <antoinebusque@gmail.com>
2017-03-21 15:42:32 -0400

    moc: add moc config
    Signed-off-by: Antoine Busque <antoinebusque@gmail.com>

--- make.sh
+++ make.sh
@@ -8,7 +8,7 @@ SRCDIR="$BASEDIR/src"
 DISTDIR="$BASEDIR/dist"
 APP="$BASEDIR/utils/app.py"
 INSTALLDIR=$HOME
-SOURCES="emacs emacs-custom.el gitconfig gitignore_global ratpoisonrc screenrc xinitrc xmobarrc Xresources zshrc zsh"
+SOURCES="emacs emacs-custom.el gitconfig gitignore_global moc ratpoisonrc screenrc xinitrc xmobarrc Xresources zshrc zsh"
 
 task_make () {
     if [ ! -z "$LAPTOP" ]; then
