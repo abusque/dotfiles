34d9435ec6407adb9b5876352da38d8cac71060f
Antoine Busque <antoinebusque@gmail.com>
2021-03-31 15:16:35 -0400

    emacs: fix terminus typeface name
    Signed-off-by: Antoine Busque <antoinebusque@gmail.com>

--- src/emacs-custom.el
+++ src/emacs-custom.el
@@ -4,7 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "gray88" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "unknown" :family "xos4 Terminus"))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "gray88" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "Terminus"))))
  '(markdown-code-face ((t (:family "terminus")))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
